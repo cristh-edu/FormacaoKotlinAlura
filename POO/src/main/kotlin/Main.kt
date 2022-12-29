352a7ddb14128aa6d667b51bfbed5d75cf6ace20
Cristhian Eduardo <contato.cristhian.slopes@gmail.com>
2022-12-29 11:35:02 -0300

    Iniciação do Projeto e Primeiros Passos

---
+++ POO/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Bem vindo ao Bytebank!\n")
+
+    val titular: String = "Cristhian" // Val = constante
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0 // var = variavel
+
+    saldo += 200
+
+    println("Titular $titular")
+    println("Número da Conta $numeroConta")
+    println("Saldo da Conta $saldo")
+}
