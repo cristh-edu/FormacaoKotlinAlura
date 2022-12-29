f030c0862ec6ef7b4cbc3821c1634040639a1a3f
Cristhian Eduardo <contato.cristhian.slopes@gmail.com>
2022-12-29 15:05:39 -0300

    Conclusão do curso

--- POO/src/main/kotlin/Main.kt
+++ POO/src/main/kotlin/Main.kt
@@ -1,62 +1,41 @@
 fun main() {
     println("Bem vindo ao Bytebank!\n")
-
+    testaClasses()
 }
 
-class Conta{
-    private var titular: String = ""
-    private var numeroConta: Int = 0
-    private var saldo: Double = 0.0
+class Conta(var titular: String, var numero: Int) {
+    var saldo: Double = 0.0
+        private set
 
-    fun getTitular(): String {
-        return titular
-    }
-
-    fun getNumeroConta(): Int {
-        return numeroConta
-    }
-
-    fun getSaldo(): Double {
-        return saldo
-    }
-
-    fun setSaldo(saldo: Double){
-        this.saldo = saldo;
-    }
-
-    constructor(titular:String , numeroConta: Int){
-        this.titular = titular;
-        this.numeroConta = numeroConta;
-    }
-
-    fun saca(valor: Double){
-        if(valor> 0) {
-            if(saldo >= valor) {
+    fun saca(valor: Double) {
+        if (valor > 0) {
+            if (saldo >= valor) {
                 this.saldo -= valor
                 println("Saque feito com sucesso!")
-            }else  println("Saldo insuficiente para saque!")
-        }else println("Deposite um valor válido!")
+            } else println("Saldo insuficiente para saque!")
+        } else println("Deposite um valor válido!")
     }
 
-    fun deposita(valor: Double){
-        if(valor> 0) {
+    fun deposita(valor: Double) {
+        if (valor > 0) {
             this.saldo += valor
             println("Deposito feito com sucesso!")
-        }else println("Deposite um valor válido!")
+        } else println("Deposite um valor válido!")
     }
 
-    fun transfere(valor: Double, destino: Conta){
-        if(valor >0){
-            if(this.saldo >= valor){
+    fun transfere(valor: Double, destino: Conta) {
+        if (valor > 0) {
+            if (this.saldo >= valor) {
                 this.saldo -= valor
                 destino.deposita(valor)
                 println("Transferência feita com sucesso!")
-            }else println("Saldo insuficiente!")
-        }else println("Transfira um valor válido!")
+            } else println("Saldo insuficiente!")
+        } else println("Transfira um valor válido!")
     }
 }
+
 //    Aula: Primeiros passos
-fun testaPrimeirosPassos(){
+fun testaPrimeirosPassos() {
     val titular: String = "Cristhian" // Val = constante
     val numeroConta: Int = 1000
     var saldo: Double = 0.0 // var = variavel
@@ -95,27 +74,29 @@ fun testaCondicoes(saldo: Double) {
 fun testaRepeticao(saldo: Double) {
     for (i in 5 downTo 1 step 2) {
         println(i)
-        if(i == 1) break
+        if (i == 1) break
         else continue
     }
     var i = 1
-    while (i<5){
+    while (i < 5) {
         i++
         println(i)
     }
 }
 
 //    Aula: Criando Classes e objetos
-fun testaClasses(){
+fun testaClasses() {
     val c1 = Conta("Cristhian", 1520)
-    c1.setSaldo(200.49)
-    println(c1.getTitular())
-    println(c1.getNumeroConta())
-    println(c1.getSaldo())
+    c1.deposita(200.49)
+    println(c1.titular)
+    println(c1.numero)
+    println(c1.saldo)
+    c1.saca(100.0)
 
-    val c2 = Conta("Eduardo",250)
-    c2.setSaldo(-20.99)
-    println(c2.getTitular())
-    println(c2.getTitular())
-    println(c2.getSaldo())
+    val c2 = Conta("Eduardo", 250)
+    c2.deposita(20.99)
+    println(c2.titular)
+    println(c2.numero)
+    println(c2.saldo)
+    c1.transfere(50.0, c2)
 }
