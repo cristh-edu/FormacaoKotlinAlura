b2e356abbe476c87cc9f9e253daf6256c54ac8c3
Cristhian Eduardo <contato.cristhian.slopes@gmail.com>
2022-12-29 13:05:33 -0300

    Cap 2. Controle de fluxo e o paradigma procedural

--- POO/src/main/kotlin/Main.kt
+++ POO/src/main/kotlin/Main.kt
@@ -6,8 +6,48 @@ fun main() {
     var saldo: Double = 0.0 // var = variavel
 
     saldo += 200
+    saldo -= 10000
 
     println("Titular $titular")
     println("Número da Conta $numeroConta")
     println("Saldo da Conta $saldo")
+    println()
+
+//    Aula: Utilizando estruturas condicionais
+//    testaCondicoes(saldo)
+
+//    Aula: Utilizando estrutura de repetição
+//    testaRepeticao(saldo)
+}
+
+fun testaCondicoes(saldo: Double) {
+    println("Testando Condições")
+
+//    if (saldo > 0.0)
+//        println("Conta é positiva")
+//    else if (saldo == 0.0)
+//        println("Conta é neutra")
+//    else
+//        println("Conta é negativa")
+
+    when {
+        saldo > 0.0 -> println("Conta é positiva")
+        saldo == 0.0 -> println("Conta é neutra")
+        else -> println("Conta é negativa")
+    }
+
+    println()
+}
+
+fun testaRepeticao(saldo: Double) {
+    for (i in 5 downTo 1 step 2) {
+        println(i)
+        if(i == 1) break
+        else continue
+    }
+    var i = 1
+    while (i<5){
+        i++
+        println(i)
+    }
 }
