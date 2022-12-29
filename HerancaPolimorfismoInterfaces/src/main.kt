8d9202e1e6c48112880910fcd122323be5e423f3
Cristhian Eduardo <contato.cristhian.slopes@gmail.com>
2022-12-29 19:26:49 -0300

    Testes e conclusão do curso

--- HerancaPolimorfismoInterfaces/src/main.kt
+++ HerancaPolimorfismoInterfaces/src/main.kt
@@ -1,14 +1,7 @@
 fun main() {
-    println("Bem vindo ao Bytebank")
-
-    val alex = Funcionario(
-        nome = "Cristhian",
-        cpf = "111.111.111-11",
-        salario = 1000.0
-    )
-
-    println("Nome ${alex.nome}")
-    println("CPF ${alex.cpf}")
-    println("Salario ${alex.salario}")
-    println("Bonificação ${alex.bonificacao}")
+//    testaComportamentosConta()
+//    testaContasDiferentes()
+//    testaCopiasEReferencias()
+//    testaFuncionarios()
+    testaAutenticacao()
 }
